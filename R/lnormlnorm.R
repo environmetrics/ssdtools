c9832f76feed116aa8b01b794bdc1d0174c4637c
joethorley <joe@poissonconsulting.ca>
2019-11-27 03:58:37 -0800

    lnormlnorm log, log.p and lower.tail

--- R/lnormlnorm.R
+++ R/lnormlnorm.R
@@ -10,38 +10,55 @@
 #' @param meanlog1,sdlog1 mean and standard deviation of the distribution on the log scale of the first mixture with default values of 0 and 1 respectively.
 #' @param meanlog2,sdlog2 mean and standard deviation of the distribution on the log scale of the second mixture with default values of 1 and 1 respectively.
 #' @param r proportion of the first mixture.
+#' @param log,log.p logical; if TRUE, probabilitise p are given as log(p).
+#' @param lower.tail logical; if TRUE (default), probabilities are \code{P[X ≤ x]}, otherwise, \code{P[X > x]}.
 #' 
 #' @name lnormlnorm
+#' @examples
+#' hist(log(rlnormlnorm(1e+05)))
 NULL
 
 #' @rdname lnormlnorm
 #' @export
-dlnormlnorm <- function(x, meanlog1 = 0, sdlog1 = 1, meanlog2 = 1, sdlog2 = 0, r = 0.5){
-  r * dlnorm(x,meanlog1,sdlog1) + (1-r)*dlnorm(x, meanlog2, sdlog2)
+dlnormlnorm <- function(x, meanlog1 = 0, sdlog1 = 1, meanlog2 = 3, sdlog2 = 1, r = 0.5, log = FALSE){
+  density <- r * dlnorm(x,meanlog1,sdlog1) + (1 - r) * dlnorm(x, meanlog2, sdlog2)
+  if(log) return(log(density))
+  density
 }
 
 #' @rdname lnormlnorm
 #' @export
-qlnormlnorm <- function(p, meanlog1 = 0, sdlog1 = 1, meanlog2 = 0, sdlog2 = 1, r = 0.5){
-  if(p==1){p=1-.Machine$double.eps}
-  if(p==0){p=0+.Machine$double.eps}
-  minmax <- c(0+.Machine$double.eps, 10^10)
-  uniroot(function(x) plnormlnorm(x, meanlog1, sdlog1, meanlog2, sdlog2, r) - p,
-          interval = minmax)$root  
+plnormlnorm <- function(q, meanlog1 = 0, sdlog1= 1, meanlog2 = 3, sdlog2 = 1, r = 0.5, lower.tail = TRUE, log.p = FALSE){
+  p <- r * plnorm(q,meanlog1, sdlog1) + (1 - r) * plnorm(q, meanlog2, sdlog2)
+  if(!lower.tail)
+    p <- 1 - p
+  if(log.p) return(log(p))
+  p
 }
 
 #' @rdname lnormlnorm
 #' @export
-plnormlnorm <- function(q, meanlog1 = 0, sdlog1= 1, meanlog2 = 1, sdlog2 = 1, r = 0.5){
-  r * plnorm(q,meanlog1, sdlog1) + (1-r)*plnorm(q, meanlog2, sdlog2)
+qlnormlnorm <- function(p, meanlog1 = 0, sdlog1 = 1, meanlog2 = 3, sdlog2 = 1, r = 0.5, lower.tail = TRUE, log.p = FALSE){
+  if(p==1) p <- 1-.Machine$double.eps
+  if(p==0) p <- 0+.Machine$double.eps
+  interval <- c(0+.Machine$double.eps, 10^10)
+  
+  f <- function(x) {
+    plnormlnorm(x, meanlog1, sdlog1, meanlog2, sdlog2, r) - p
+  }
+  q <- uniroot(f, interval = interval)$root
+  if(!lower.tail) q <- 1 - q
+  if(log.p) return(log(q))
+  q
 }
 
 #' @rdname lnormlnorm
 #' @export
-rlnormlnorm <- function(n, meanlog1 = 0, sdlog1 = 1, meanlog2 = 1, sdlog2 = 1, r = 0.5){
-  n.lnorm1 <- rbinom(1, size = n, prob = r)
-  n.lnorm2 <- n - n.lnorm1
-  sample <- c(rlnorm(n=n.lnorm1, meanlog1, sdlog1), 
-              rlnorm(n=n.lnorm2, meanlog2, sdlog2))
+rlnormlnorm <- function(n, meanlog1 = 0, sdlog1 = 1, meanlog2 = 3, sdlog2 = 1, r = 0.5) {
+  if(length(n) > 1) n <- length(n)
+  n1 <- rbinom(1, size = n, prob = r)
+  n2 <- n - n1
+  sample <- c(rlnorm(n1, meanlog1, sdlog1), 
+              rlnorm(n2, meanlog2, sdlog2))
   sample(sample, size = n)
 }
--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -75,27 +75,3 @@ test_that("fluazinam", {
   expect_true(is.fitdistcens(dist))
   expect_equal(coef(dist), c(meanlog = 3.566750, sdlog = 2.182757), tolerance = 0.000001)
 })
-
-test_that("lnormlnorm", {
-  data <- ssdtools::boron_data
-  dist <- ssdtools:::ssd_fit_dist(data, dist = "lnormlnorm")
-  
-  expect_true(is.fitdist(dist))
-  expect_false(is.fitdistcens(dist))
-  expect_equal(coef(dist), c(meanlog1 = 0.949706713910721, sdlog1 = 0.55446333681171, meanlog2 = 3.20119275002971, 
-sdlog2 = 0.768836125716127, r = 0.283997185736815))
-
-  dist <- ssd_fit_dist(fluazinam, left = "left", right = "right")
-  expect_false(is.fitdist(dist))
-  expect_true(is.fitdistcens(dist))
-  expect_equal(coef(dist), c(meanlog = 4.976920, sdlog = 2.687785), tolerance = 0.0000001)
-
-  fluazinam2 <- fluazinam[rev(order(fluazinam$left)), ]
-  fluazinam2$Weight <- 1:nrow(fluazinam2)
-
-  dist <- ssd_fit_dist(fluazinam2, weight = "Weight", left = "left", right = "right")
-  expect_false(is.fitdist(dist))
-  expect_true(is.fitdistcens(dist))
-  expect_equal(coef(dist), c(meanlog = 3.566750, sdlog = 2.182757), tolerance = 0.000001)
-})
-
